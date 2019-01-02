d1bc688afce18dea9045c5c4c9d34764057a110f
ddd503 <skbsporta7@gmail.com>
2019-01-03 02:46:51 +0900

    Environment preparation

--- AccordionMenu-MVVM/View/ViewController.swift
+++ AccordionMenu-MVVM/View/ViewController.swift
@@ -8,13 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        bindViewModel()
     }
 
+    private func bindViewModel() {}
 
 }
 
--- AccordionMenu-MVVM/ViewModel/ViewModel.swift
+++ AccordionMenu-MVVM/ViewModel/ViewModel.swift
@@ -6,4 +6,26 @@
 //  Copyright © 2019 kawaharadai. All rights reserved.
 //
 
+protocol ViewModelInputs: class {
+
+}
+
+protocol ViewModelOutputs: class {
+
+}
+
+protocol ViewModelType {
+    var inputs: ViewModelInputs { get }
+    var outputs: ViewModelOutputs { get }
+}
+
 final class ViewModel {}
+
+extension ViewModel: ViewModelType {
+    var inputs: ViewModelInputs { return self }
+    var outputs: ViewModelOutputs { return self }
+}
+
+extension ViewModel: ViewModelInputs {}
+
+extension ViewModel: ViewModelOutputs {}
