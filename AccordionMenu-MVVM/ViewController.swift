36d274a53cf674f4c4d636fdd1306827046a769a
ddd503 <skbsporta7@gmail.com>
2019-01-02 21:44:25 +0900

    Initial commit

---
+++ AccordionMenu-MVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AccordionMenu-MVVM
+//
+//  Created by kawaharadai on 2019/01/02.
+//  Copyright © 2019 kawaharadai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
