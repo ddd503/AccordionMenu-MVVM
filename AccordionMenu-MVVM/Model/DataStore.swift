9bc0ababbdbf547aa665871db1e94dd635c100e6
ddd503 <skbsporta7@gmail.com>
2019-01-02 22:07:26 +0900

    organize directry

---
+++ AccordionMenu-MVVM/Model/DataStore.swift
@@ -0,0 +1,9 @@
+//
+//  DataStore.swift
+//  AccordionMenu-MVVM
+//
+//  Created by kawaharadai on 2019/01/02.
+//  Copyright © 2019 kawaharadai. All rights reserved.
+//
+
+final class DataStore {}
---
+++ AccordionMenu-MVVM/ViewModel/ViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  ViewModel.swift
+//  AccordionMenu-MVVM
+//
+//  Created by kawaharadai on 2019/01/02.
+//  Copyright © 2019 kawaharadai. All rights reserved.
+//
+
+final class ViewModel {}
